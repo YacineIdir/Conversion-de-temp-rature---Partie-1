7c197bb3f2d7a4b9420288a4e8460916477e988d
YacineIdir <146662613+YacineIdir@users.noreply.github.com>
2024-08-23 10:47:06 -0400

    EXERCICE

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+string Celcius;
+double Kelvin, Farheinet;
+
+Console.WriteLine("Veuillez rentrer la temperature en Celcius: ");
+
+Celcius = Console.ReadLine();
+
+Kelvin = (Convert.ToInt64(Celcius) + 273.15);
+Farheinet = (Convert.ToInt64(Celcius) * 9/5 + 32);
+
+Console.WriteLine("Celcius: " + Celcius + "°C");
+Console.WriteLine("Kelvin: " + Kelvin + "°K");
+Console.WriteLine("Farheinet: " + Farheinet + "°F");
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Conversion-de-temp-rature---Partie-1
+INPUT
+int Celcius;
+OUTPUT
+double int Kelvin;
+double int Farheinet;
+
+DEBUT
+
+ECRIRE "Veuillez rentrer la temperature en Celcius"
+LIRE Celcius
+ECRIRE Celcius, kelvin et Farheinet
+
+FIN
